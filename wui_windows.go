c30c0b0bcef2f762603cc86f240a598ecbbe28a0
Lars <lars.scheme@gmail.com>
2018-08-30 16:25:37 +0200

    Add message boxes

--- wui_windows.go
+++ wui_windows.go
@@ -14,6 +14,22 @@ import (
 	"github.com/gonutz/w32"
 )
 
+func MessageBox(caption, text string) {
+	w32.MessageBox(0, text, caption, w32.MB_OK|w32.MB_TOPMOST)
+}
+
+func MessageBoxError(caption, text string) {
+	w32.MessageBox(0, text, caption, w32.MB_OK|w32.MB_ICONERROR|w32.MB_TOPMOST)
+}
+
+func MessageBoxOKCancel(caption, text string) bool {
+	return w32.MessageBox(0, text, caption, w32.MB_OKCANCEL|w32.MB_TOPMOST) == w32.IDOK
+}
+
+func MessageBoxYesNo(caption, text string) bool {
+	return w32.MessageBox(0, text, caption, w32.MB_YESNO|w32.MB_TOPMOST) == w32.IDYES
+}
+
 type Control interface {
 	isControl()
 }
