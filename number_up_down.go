167d8f0996a917d794232789b3bc97b13a248af5
Lars <lars.scheme@gmail.com>
2018-09-10 14:10:29 +0200

    Fix NumberUpDown

--- number_up_down.go
+++ number_up_down.go
@@ -17,28 +17,23 @@ func NewNumberUpDown() *NumberUpDown {
 }
 
 type NumberUpDown struct {
+	textControl
 	upDownHandle  w32.HWND
-	editHandle    w32.HWND
-	x             int
-	y             int
-	width         int
-	height        int
-	parent        container
 	value         int32
 	minValue      int32
 	maxValue      int32
-	disabled      bool
-	hidden        bool
 	onValueChange func(value int)
 }
 
-func (*NumberUpDown) isControl() {}
-
-func (n *NumberUpDown) setParent(parent container) {
-	n.parent = parent
-}
-
 func (n *NumberUpDown) create(id int) {
+	// the main handle is for the edit field
+	n.text = strconv.Itoa(int(n.value))
+	n.textControl.create(
+		id,
+		w32.WS_EX_CLIENTEDGE,
+		"EDIT",
+		w32.WS_TABSTOP|w32.ES_NUMBER,
+	)
 	var visible uint
 	if !n.hidden {
 		visible = w32.WS_VISIBLE
@@ -51,18 +46,7 @@ func (n *NumberUpDown) create(id int) {
 		n.x, n.y, n.width, n.height,
 		n.parent.getHandle(), 0, n.parent.getInstance(), nil,
 	)
-	edit := w32.CreateWindowExStr(
-		w32.WS_EX_CLIENTEDGE,
-		"EDIT",
-		strconv.Itoa(int(n.value)),
-		visible|w32.WS_CHILD|w32.WS_TABSTOP|w32.ES_NUMBER,
-		n.x, n.y, n.width, n.height,
-		n.parent.getHandle(), w32.HMENU(id), n.parent.getInstance(), nil,
-	)
-	if n.disabled {
-		w32.EnableWindow(edit, false)
-	}
-	w32.SendMessage(upDown, w32.UDM_SETBUDDY, uintptr(edit), 0)
+	w32.SendMessage(upDown, w32.UDM_SETBUDDY, uintptr(n.handle), 0)
 	w32.SendMessage(
 		upDown,
 		w32.UDM_SETRANGE32,
@@ -70,32 +54,18 @@ func (n *NumberUpDown) create(id int) {
 		uintptr(n.maxValue),
 	)
 	n.upDownHandle = upDown
-	n.editHandle = edit
-	// TODO handle font
-	//if n.parent.font != nil {
-	//	w32.SendMessage(
-	//		edit,
-	//		w32.WM_SETFONT,
-	//		uintptr(n.parent.font.handle),
-	//		1,
-	//	)
-	//}
 }
 
-func (n *NumberUpDown) parentFontChanged() {
-	// TODO
-}
-
-func (n *NumberUpDown) Enabled() bool {
-	return !n.disabled
-}
-
-func (n *NumberUpDown) SetEnabled(e bool) *NumberUpDown {
-	n.disabled = !e
-	if n.editHandle != 0 {
-		w32.EnableWindow(n.editHandle, e)
+func (n *NumberUpDown) SetBounds(x, y, width, height int) {
+	n.textControl.SetBounds(x, y, width, height)
+	if n.upDownHandle != 0 {
+		w32.SetWindowPos(
+			n.upDownHandle, 0,
+			n.x, n.y, n.width, n.height,
+			w32.SWP_NOOWNERZORDER|w32.SWP_NOZORDER,
+		)
+		w32.SendMessage(n.upDownHandle, w32.UDM_SETBUDDY, uintptr(n.handle), 0)
 	}
-	return n
 }
 
 func (n *NumberUpDown) Value() int {
@@ -105,7 +75,7 @@ func (n *NumberUpDown) Value() int {
 	return int(n.value)
 }
 
-func (n *NumberUpDown) SetValue(v int) *NumberUpDown {
+func (n *NumberUpDown) SetValue(v int) {
 	n.value = int32(v)
 	if n.value < n.minValue {
 		n.value = n.minValue
@@ -116,10 +86,13 @@ func (n *NumberUpDown) SetValue(v int) *NumberUpDown {
 	if n.upDownHandle != 0 {
 		w32.SendMessage(n.upDownHandle, w32.UDM_SETPOS32, 0, uintptr(v))
 	}
-	return n
 }
 
-func (n *NumberUpDown) SetMinValue(min int) *NumberUpDown {
+func (n *NumberUpDown) MinValue() int {
+	return int(n.minValue)
+}
+
+func (n *NumberUpDown) SetMinValue(min int) {
 	if n.Value() < min {
 		n.SetValue(min)
 	}
@@ -132,10 +105,13 @@ func (n *NumberUpDown) SetMinValue(min int) *NumberUpDown {
 			uintptr(n.maxValue),
 		)
 	}
-	return n
 }
 
-func (n *NumberUpDown) SetMaxValue(max int) *NumberUpDown {
+func (n *NumberUpDown) MaxValue() int {
+	return int(n.maxValue)
+}
+
+func (n *NumberUpDown) SetMaxValue(max int) {
 	if n.Value() > max {
 		n.SetValue(max)
 	}
@@ -148,10 +124,13 @@ func (n *NumberUpDown) SetMaxValue(max int) *NumberUpDown {
 			uintptr(n.maxValue),
 		)
 	}
-	return n
 }
 
-func (n *NumberUpDown) SetMinMaxValues(min, max int) *NumberUpDown {
+func (n *NumberUpDown) MinMaxValues() (min, max int) {
+	return int(n.minValue), int(n.maxValue)
+}
+
+func (n *NumberUpDown) SetMinMaxValues(min, max int) {
 	if n.Value() < min {
 		n.SetValue(min)
 	} else if n.Value() > max {
@@ -167,33 +146,6 @@ func (n *NumberUpDown) SetMinMaxValues(min, max int) *NumberUpDown {
 			uintptr(n.maxValue),
 		)
 	}
-	return n
-}
-
-func (n *NumberUpDown) SetBounds(x, y, width, height int) *NumberUpDown {
-	n.x = x
-	n.y = y
-	n.width = width
-	n.height = height
-	if n.editHandle != 0 {
-		w32.SetWindowPos(
-			n.editHandle, 0,
-			n.x, n.y, n.width, n.height,
-			w32.SWP_NOOWNERZORDER|w32.SWP_NOZORDER,
-		)
-		w32.SetWindowPos(
-			n.upDownHandle, 0,
-			n.x, n.y, n.width, n.height,
-			w32.SWP_NOOWNERZORDER|w32.SWP_NOZORDER,
-		)
-		w32.SendMessage(
-			n.upDownHandle,
-			w32.UDM_SETBUDDY,
-			uintptr(n.editHandle),
-			0,
-		)
-	}
-	return n
 }
 
 func (n *NumberUpDown) SetOnValueChange(f func(value int)) *NumberUpDown {
