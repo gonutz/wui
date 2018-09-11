e4a5a49cb37d98df6d6cad7eb994f393ec1aeb75
Lars <lars.scheme@gmail.com>
2018-09-11 14:06:56 +0200

    Add parent to message boxes

--- message_box.go
+++ message_box.go
@@ -4,18 +4,26 @@ package wui
 
 import "github.com/gonutz/w32"
 
-func MessageBox(caption, text string) {
-	w32.MessageBox(0, text, caption, w32.MB_OK|w32.MB_TOPMOST)
+func MessageBox(parent *Window, caption, text string) {
+	msgBox(parent, caption, text, w32.MB_OK)
 }
 
-func MessageBoxError(caption, text string) {
-	w32.MessageBox(0, text, caption, w32.MB_OK|w32.MB_ICONERROR|w32.MB_TOPMOST)
+func MessageBoxError(parent *Window, caption, text string) {
+	msgBox(parent, caption, text, w32.MB_OK|w32.MB_ICONERROR)
 }
 
-func MessageBoxOKCancel(caption, text string) bool {
-	return w32.MessageBox(0, text, caption, w32.MB_OKCANCEL|w32.MB_TOPMOST) == w32.IDOK
+func MessageBoxOKCancel(parent *Window, caption, text string) bool {
+	return msgBox(parent, caption, text, w32.MB_OKCANCEL) == w32.IDOK
 }
 
-func MessageBoxYesNo(caption, text string) bool {
-	return w32.MessageBox(0, text, caption, w32.MB_YESNO|w32.MB_TOPMOST) == w32.IDYES
+func MessageBoxYesNo(parent *Window, caption, text string) bool {
+	return msgBox(parent, caption, text, w32.MB_YESNO) == w32.IDYES
+}
+
+func msgBox(parent *Window, caption, text string, flags uint) int {
+	var handle w32.HWND
+	if parent != nil {
+		handle = parent.handle
+	}
+	return w32.MessageBox(handle, text, caption, w32.MB_TOPMOST|flags)
 }
