112f1472f65ebb192536a48d475f8a94538ebd31
Lars <lars.scheme@gmail.com>
2018-09-12 14:10:31 +0200

    Text edit: add character limit

--- text_edit.go
+++ text_edit.go
@@ -10,6 +10,7 @@ func NewTextEdit() *TextEdit {
 
 type TextEdit struct {
 	textControl
+	limit int
 }
 
 func (e *TextEdit) create(id int) {
@@ -19,4 +20,21 @@ func (e *TextEdit) create(id int) {
 			w32.ES_LEFT|w32.ES_MULTILINE|w32.ES_AUTOVSCROLL|w32.ES_AUTOHSCROLL|
 			w32.ES_WANTRETURN,
 	)
+	if e.limit != 0 {
+		e.SetCharacterLimit(e.limit)
+	}
+}
+
+func (e *TextEdit) SetCharacterLimit(count int) {
+	e.limit = count
+	if e.handle != 0 {
+		w32.SendMessage(e.handle, w32.EM_SETLIMITTEXT, uintptr(e.limit), 0)
+	}
+}
+
+func (e *TextEdit) CharacterLimit() int {
+	if e.handle != 0 {
+		e.limit = int(w32.SendMessage(e.handle, w32.EM_GETLIMITTEXT, 0, 0))
+	}
+	return e.limit
 }
