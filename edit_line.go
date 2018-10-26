e1cf7088db48bff8ef6a3236aadf7beb0bb40b60
Lars <lars.scheme@gmail.com>
2018-10-26 17:20:02 +0200

    Add EditLine.SetPassword to obfuscate text

--- edit_line.go
+++ edit_line.go
@@ -10,6 +10,8 @@ func NewEditLine() *EditLine {
 
 type EditLine struct {
 	textControl
+	isPass   bool
+	passChar uintptr
 }
 
 func (e *EditLine) create(id int) {
@@ -17,6 +19,24 @@ func (e *EditLine) create(id int) {
 		id,
 		w32.WS_EX_CLIENTEDGE,
 		"EDIT",
-		w32.WS_TABSTOP|w32.ES_AUTOHSCROLL,
+		w32.WS_TABSTOP|w32.ES_AUTOHSCROLL|w32.ES_PASSWORD,
 	)
+	e.passChar = w32.SendMessage(e.handle, w32.EM_GETPASSWORDCHAR, 0, 0)
+	e.SetPassword(e.isPass)
+}
+
+func (e *EditLine) SetPassword(isPass bool) {
+	e.isPass = isPass
+	if e.handle != 0 {
+		if e.isPass {
+			w32.SendMessage(e.handle, w32.EM_SETPASSWORDCHAR, e.passChar, 0)
+		} else {
+			w32.SendMessage(e.handle, w32.EM_SETPASSWORDCHAR, 0, 0)
+		}
+		w32.InvalidateRect(e.parent.getHandle(), nil, true)
+	}
+}
+
+func (e *EditLine) IsPassword() bool {
+	return e.isPass
 }
