c03fa4b9a9f38ac838ba5b31013bd945e47daece
Lars <lars.scheme@gmail.com>
2019-01-22 13:13:44 +0100

    Add TextEdit.SetWordWrap to set auto horizontal scroll

--- text_edit.go
+++ text_edit.go
@@ -9,19 +9,24 @@ import (
 )
 
 func NewTextEdit() *TextEdit {
-	return &TextEdit{}
+	return &TextEdit{autoHScroll: true}
 }
 
 type TextEdit struct {
 	textControl
-	limit int
+	limit       int
+	autoHScroll bool
 }
 
 func (e *TextEdit) create(id int) {
+	var hScroll uint
+	if e.autoHScroll {
+		hScroll |= w32.ES_AUTOHSCROLL
+	}
 	e.textControl.create(
 		id, w32.WS_EX_CLIENTEDGE, "EDIT",
 		w32.WS_TABSTOP|w32.WS_VSCROLL|
-			w32.ES_LEFT|w32.ES_MULTILINE|w32.ES_AUTOVSCROLL|w32.ES_AUTOHSCROLL|
+			w32.ES_LEFT|w32.ES_MULTILINE|w32.ES_AUTOVSCROLL|hScroll|
 			w32.ES_WANTRETURN,
 	)
 	if e.limit != 0 {
@@ -63,4 +68,15 @@ func (e *TextEdit) CharacterLimit() int {
 		e.limit = int(w32.SendMessage(e.handle, w32.EM_GETLIMITTEXT, 0, 0))
 	}
 	return e.limit
+}
+
+func (e *TextEdit) SetWordWrap(wrap bool) {
+	if e.handle == 0 {
+		// the ES_AUTOHSCROLL style cannot be changed at runtime
+		e.autoHScroll = !wrap
+	}
+}
+
+func (e *TextEdit) WordWrap() bool {
+	return !e.autoHScroll
 }
