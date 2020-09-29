1a3105589f3f0904e90d45efdbf9330213c7f1bd
lars <lars.scheme@gmail.com>
2020-09-29 14:21:09 +0200

    Rename Combobox to ComboBox

--- combobox.go
+++ combobox.go
@@ -9,18 +9,18 @@ import (
 	"github.com/gonutz/w32"
 )
 
-func NewCombobox() *Combobox {
-	return &Combobox{selected: -1}
+func NewComboBox() *ComboBox {
+	return &ComboBox{selected: -1}
 }
 
-type Combobox struct {
+type ComboBox struct {
 	textControl
 	items    []string
 	selected int
 	onChange func(newIndex int)
 }
 
-func (e *Combobox) create(id int) {
+func (e *ComboBox) create(id int) {
 	e.textControl.create(
 		id,
 		w32.WS_EX_CLIENTEDGE,
@@ -33,37 +33,37 @@ func (e *Combobox) create(id int) {
 	e.SetSelectedIndex(e.selected)
 }
 
-func (e *Combobox) Add(s string) {
+func (e *ComboBox) Add(s string) {
 	e.items = append(e.items, s)
 	if e.handle != 0 {
 		e.addItem(s)
 	}
 }
 
-func (e *Combobox) addItem(s string) {
+func (e *ComboBox) addItem(s string) {
 	ptr, _ := syscall.UTF16PtrFromString(s)
 	w32.SendMessage(e.handle, w32.CB_ADDSTRING, 0, uintptr(unsafe.Pointer(ptr)))
 }
 
-func (e *Combobox) Clear() {
+func (e *ComboBox) Clear() {
 	e.items = nil
 	if e.handle != 0 {
 		w32.SendMessage(e.handle, w32.CB_RESETCONTENT, 0, 0)
 	}
 }
 
-func (e *Combobox) Items() []string {
+func (e *ComboBox) Items() []string {
 	return e.items
 }
 
-func (e *Combobox) SelectedIndex() int {
+func (e *ComboBox) SelectedIndex() int {
 	if e.handle != 0 {
 		e.selected = int(w32.SendMessage(e.handle, w32.CB_GETCURSEL, 0, 0))
 	}
 	return e.selected
 }
 
-func (e *Combobox) SetSelectedIndex(i int) {
+func (e *ComboBox) SetSelectedIndex(i int) {
 	oldI := e.selected
 	if 0 <= i && i < len(e.items) {
 		e.selected = i
@@ -78,11 +78,11 @@ func (e *Combobox) SetSelectedIndex(i int) {
 	}
 }
 
-func (e *Combobox) SetOnChange(f func(newIndex int)) {
+func (e *ComboBox) SetOnChange(f func(newIndex int)) {
 	e.onChange = f
 }
 
-func (e *Combobox) handleNotification(cmd uintptr) {
+func (e *ComboBox) handleNotification(cmd uintptr) {
 	if cmd == w32.CBN_SELCHANGE && e.onChange != nil {
 		e.onChange(e.SelectedIndex())
 	}
