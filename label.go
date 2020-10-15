08950f66f138bd62541e5f0d7317467310c05923
lars <lars.scheme@gmail.com>
2020-10-15 16:52:56 +0200

    Label: replace alignment functions with SetAlignment

--- label.go
+++ label.go
@@ -5,49 +5,50 @@ package wui
 import "github.com/gonutz/w32"
 
 func NewLabel() *Label {
-	return &Label{align: w32.SS_LEFT}
+	return &Label{}
 }
 
 type Label struct {
 	textControl
-	align uint
+	alignment TextAlignment
 }
+
+type TextAlignment int
+
+const (
+	AlignLeft TextAlignment = iota
+	AlignCenter
+	AlignRight
+)
 
 func (l *Label) create(id int) {
-	l.textControl.create(id, 0, "STATIC", w32.SS_CENTERIMAGE|l.align)
+	l.textControl.create(id, 0, "STATIC", w32.SS_CENTERIMAGE|alignStyle(l.alignment))
 }
 
-func (l *Label) setAlign(align uint) *Label {
-	l.align = align
+func alignStyle(a TextAlignment) uint {
+	if a == AlignCenter {
+		return w32.SS_CENTER
+	}
+	if a == AlignRight {
+		return w32.SS_RIGHT
+	}
+	return w32.SS_LEFT
+}
+
+func (l *Label) SetAlignment(a TextAlignment) {
+	l.alignment = a
 	if l.handle != 0 {
 		style := uint(w32.GetWindowLongPtr(l.handle, w32.GWL_STYLE))
 		style = style &^ w32.SS_LEFT &^ w32.SS_CENTER &^ w32.SS_RIGHT
-		w32.SetWindowLongPtr(l.handle, w32.GWL_STYLE, uintptr(style|l.align))
+		w32.SetWindowLongPtr(
+			l.handle,
+			w32.GWL_STYLE,
+			uintptr(style|alignStyle(l.alignment)),
+		)
 		w32.InvalidateRect(l.handle, nil, true)
 	}
-	return l
 }
 
-func (l *Label) SetLeftAlign() *Label {
-	return l.setAlign(w32.SS_LEFT)
-}
-
-func (l *Label) SetCenterAlign() *Label {
-	return l.setAlign(w32.SS_CENTER)
-}
-
-func (l *Label) SetRightAlign() *Label {
-	return l.setAlign(w32.SS_RIGHT)
-}
-
-func (l *Label) IsLeftAligned() bool {
-	return l.align == w32.SS_LEFT
-}
-
-func (l *Label) IsCenterAligned() bool {
-	return l.align == w32.SS_CENTER
-}
-
-func (l *Label) IsRightAligned() bool {
-	return l.align == w32.SS_RIGHT
+func (l *Label) Alignment() TextAlignment {
+	return l.alignment
 }
