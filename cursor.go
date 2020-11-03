9a212721f659f590f08bd6750b24574c0fe5dabb
gonutz <lars.scheme@gmail.com>
2020-11-03 14:48:38 +0100

    Fix and document CursorFromImage

--- cursor.go
+++ cursor.go
@@ -34,7 +34,28 @@ func loadCursor(id uint16) *Cursor {
 	return &Cursor{handle: w32.LoadCursor(0, w32.MakeIntResource(id))}
 }
 
+// CursorFromImage creates a cursor with 4 possible colors: black, white,
+// transparent and inverse screen color. Inverse screen color depends on what is
+// under the cursor at any time. The cursor will be white minus the screen
+// color to give maximum contrast for such pixels.
+//
+// Fully opaque black and white pixels in the image are interpreted as black and
+// white.
+//
+// Fully transparent pixels in the image are interpreted as screen color.
+//
+// All other pixels are interpreted as inverse screen color. This means even
+// "almost" black/white/transparent, say with intensity 0xFFFE instead of
+// 0xFFFF, will be interpreted as inverse screen color.
 func CursorFromImage(img image.Image, x, y int) (*Cursor, error) {
+	// Cursor images have two bit masks: AND and XOR. Combining the bits in both
+	// will yield these results:
+	//
+	// AND  XOR  Result
+	//  0    0   Black
+	//  0    1   White
+	//  1    0   Screen color (transparent)
+	//  1    1   Inverse screen color
 	b := img.Bounds()
 	bits := make([]byte, b.Dx()*b.Dy()*2)
 	and := bits[:len(bits)/2]
@@ -51,14 +72,19 @@ func CursorFromImage(img image.Image, x, y int) (*Cursor, error) {
 			i := count / 8
 			count++
 			mask = mask>>1 | mask<<7
-			if a < halfIntensity {
-				// Transparent -> AND=1 XOR=0.
+			if a == 0 {
+				// Alpha is 0 -> use screen color.
 				and[i] |= mask
-			} else if r+g+b >= 3*halfIntensity {
-				// White -> AND=0 XOR=1.
+			} else if r == 0 && g == 0 && b == 0 && a == 0xFFFF {
+				// Black pixel. Nothing to change here, 0 0 is the default.
+			} else if r == 0xFFFF && g == 0xFFFF && b == 0xFFFF && a == 0xFFFF {
+				// White pixel.
+				xor[i] |= mask
+			} else {
+				// Everything else is inverse screen color.
+				and[i] |= mask
 				xor[i] |= mask
 			}
-			// Otherwise we assume black which has AND=0 XOR=0 so we do nothing.
 		}
 	}
 	handle := w32.CreateCursor(w32.GetModuleHandle(""), x, y, b.Dx(), b.Dy(), and, xor)
