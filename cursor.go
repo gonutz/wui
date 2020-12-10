747d0d6ad373c47ace09eec678f9fa8996c2bbd5
lars <lars.scheme@gmail.com>
2020-12-10 19:15:15 +0100

    Rename CursorSizeALL to CursorSizeAll

--- cursor.go
+++ cursor.go
@@ -16,37 +16,50 @@ type Cursor struct {
 var (
 	// CursorArrow is the standard, default arrow cursor.
 	CursorArrow = loadCursor(w32.IDC_ARROW)
+
 	// CursorIBeam is the text cursor, it looks like the letter I.
 	CursorIBeam = loadCursor(w32.IDC_IBEAM)
+
 	// CursorWait is the hour glass or rotating circle cursor that indicates
 	// that an action will take some more time.
 	CursorWait = loadCursor(w32.IDC_WAIT)
+
 	// CursorCross looks like a black + (plus) symbol.
 	CursorCross = loadCursor(w32.IDC_CROSS)
+
 	// CursorUpArrow is a vertical arrow pointing upwards.
 	CursorUpArrow = loadCursor(w32.IDC_UPARROW)
+
 	// CursorSizeNWSE is a diagonal line from top-left to bottom-right with
 	// arrows at both ends.
 	CursorSizeNWSE = loadCursor(w32.IDC_SIZENWSE)
+
 	// CursorSizeNESW is a diagonal line from top-right to bottom-left with
 	// arrows at both ends.
 	CursorSizeNESW = loadCursor(w32.IDC_SIZENESW)
+
 	// CursorSizeWE is a horizontal line from left to right with arrows at both
 	// ends.
 	CursorSizeWE = loadCursor(w32.IDC_SIZEWE)
+
 	// CursorSizeNS is a vertical line from top to bottom with arrows at both
 	// ends.
 	CursorSizeNS = loadCursor(w32.IDC_SIZENS)
-	// CursorSizeALL is a white + (plus) symbol with arrows at all four ends.
-	CursorSizeALL = loadCursor(w32.IDC_SIZEALL)
+
+	// CursorSizeAll is a white + (plus) symbol with arrows at all four ends.
+	CursorSizeAll = loadCursor(w32.IDC_SIZEALL)
+
 	// CursorNo indicates that an action is not possible. It is a crossed-out
 	// red circle, like a stop sign.
 	CursorNo = loadCursor(w32.IDC_NO)
+
 	// CursorHand is a hand pointing its index finger upwards.
 	CursorHand = loadCursor(w32.IDC_HAND)
+
 	// CursorAppStarting is a combination of CursorArrow and CursorWait, it has
 	// the arrow cursor but with an hour glass or rotating circle next to it.
 	CursorAppStarting = loadCursor(w32.IDC_APPSTARTING)
+
 	// CursorHelp is the default arrow cursor with a little question mark icon
 	// next to it.
 	CursorHelp = loadCursor(w32.IDC_HELP)
